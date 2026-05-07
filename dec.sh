e3a57eb200c922a377e12a154a0dbb2421564202
Mikołaj Maciejak <myster123987@gmail.com>
2026-05-07 17:03:02 +0200

    add: enc.sh and dec.sh scripts with new age key

---
+++ dec.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+if [ $# -ne 1 ]; then echo "Usage: dec <file>" >&2; exit 1; fi
+
+KEY="./age.agekey"
+FILE="$1"
+
+if [ ! -f "$KEY" ]; then echo "Error: $KEY not found" >&2; exit 1; fi
+if [ ! -f "$FILE" ]; then echo "Error: $FILE not found" >&2; exit 1; fi
+
+SOPS_AGE_KEY_FILE="$KEY" sops --in-place --input-type yaml --output-type yaml --decrypt "$FILE"
